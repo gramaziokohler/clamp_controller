ed51bf410ebb3377e997f16433b31cea475518e5
Victor Leung <yck011522@gmail.com>
2021-10-16 12:39:43 +0200

    Getting rid of RSTP server code, SoftAP code.

--- camera_firmware/esp32cam_firmware/wifikeys.h
+++ camera_firmware/esp32cam_firmware/wifikeys.h
@@ -1,2 +1,4 @@
-const char *ssid =  "itj";         // Put your SSID here
-const char *password = "integral_timber_joints";         // Put your PASSWORD here
+const char *ssid =  "WN-18B284";         // Put your SSID here
+const char *password = "8abfadf2ba";         // Put your PASSWORD here
+//const char *ssid =  "itj";         // Put your SSID here
+//const char *password = "integral_timber_joints";         // Put your PASSWORD here
