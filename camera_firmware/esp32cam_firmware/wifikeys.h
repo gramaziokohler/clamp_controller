d810d57717c4fe6a37949cc5df035a5f81a160f7
Victor Leung <yck011522@gmail.com>
2021-10-18 12:42:14 +0200

    moved to https://github.com/gramaziokohler/clamp_firmware

--- camera_firmware/esp32cam_firmware/wifikeys.h
+++
@@ -1,4 +0,0 @@
-const char *ssid =  "WN-18B284";         // Put your SSID here
-const char *password = "8abfadf2ba";         // Put your PASSWORD here
-//const char *ssid =  "itj";         // Put your SSID here
-//const char *password = "integral_timber_joints";         // Put your PASSWORD here
